f9ae4fbcffb18047f8a9d31cb6a6d321511a17c8
sharabeshj <sharabeshjayaraman@gmail.com>
2017-08-09 21:58:09 +0530

    preventing sql injection

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const express=require('express');
 const path=require('path');
 const http=require('http');
 const socketIO=require('socket.io');
-const bodyParser=require('body-parser');
+//const bodyParser=require('body-parser');
 const mysql=require('mysql');
 var form=express();
 var publicPath=path.join(__dirname,'../public');
 //console.log(publicPath);
 var server=http.createServer(form);
 var io=socketIO(server);
-form.use(bodyParser.urlencoded({extended:true}) );
+// form.use(bodyParser.urlencoded({extended:true}) );
 form.use(express.static(publicPath));
 form.get('/',function(req,res){
   res.sendFile(publicPath+'/form.html');
@@ -17,14 +17,15 @@ form.get('/',function(req,res){
 form.post('/',function(req,res){
 //  console.log(req.query);
 
+console.log(req.body);
 
-var name=stringi(req.query.name);
-var number=stringi(req.query.number);
-var email=stringi(req.query.email)
-var events=stringi(req.query.event);
-var department=stringi(req.query.department);
+var name=req.query.name;
+var number=req.query.number;
+var email=req.query.email;
+var events=req.query.event;
+var department=req.query.department;
 var year=req.query.year;
-var resume=stringi(req.query.resume);
+var resume=req.query.resume;
 
 var con = mysql.createConnection({
   host: "127.0.0.1",
@@ -40,19 +41,21 @@ con.connect(function(err) {
 //  console.log(("'"'+`${req.query.name}`+'"'"));
 //  console.log(req.query);
 //  console.log("Yo", stringi(req.query.name));
-  var sql = `INSERT INTO FORM (NAME,NUMBER,EMAIL,DEPARTMENT,YEAR,EVENT,RESUME) VALUES (${name},${number},${email},${department},${year},${events},${resume})`;
-  con.query(sql, function (err, result) {
+  var sql = `INSERT INTO FORM (NAME,NUMBER,EMAIL,DEPARTMENT,YEAR,EVENT,RESUME) VALUES (?,?,?,?,?,?,?)`;
+  con.query(sql,[name,number,email,department,year,events,resume], function (err, result, fields) {
     if (err) throw err;
     console.log("1 record inserted");
+
+    console.log("fields", fields);
 });
 });
 res.send("Thanks for submitting!!");
 res.end();
 });
 
-function stringi(variable) {
+/*function stringi(variable) {
   return '"' + variable + '"';
-}
+}*/
 /*form.post('/shara',function(req,res) {
   console.log(req.body);
 })*/
@@ -75,11 +78,12 @@ io.on('connection',function(socket) {
       if (err) throw err;
       con.query("SELECT * FROM FORM", function (err, result, fields) {
         if (err) throw err;
-        socket.emit('result', result);
+        io.emit('result', result);
       });
     });
   });
 });
+
 server.listen(3000,function() {
   console.log('port open');
-})
+});
